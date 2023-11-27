51d4b0381e4a500f286ccb4e78926db3c168d8b1
Anthony Hanel <ahanel13@gmail.com>
2023-11-26 21:43:54 -0700

    Added a menu context that will create a new instance of ScanTab

--- src/main/java/controller/UploadScannerController.java
+++ src/main/java/controller/UploadScannerController.java
@@ -1,27 +1,52 @@
 package controller;
 
 import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.ui.contextmenu.ContextMenuItemsProvider;
 import controller.tabControllers.GlobalConfigController;
 import model.ConfigModel;
+import view.UploadScannerMenuContext;
 import view.UploadScannerPanel;
 import view.tabs.AboutTab;
+import view.tabs.ScanTab;
 
-public class UploadScannerController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-    public UploadScannerController(MontoyaApi montoyaApi, UploadScannerPanel scannerPanel) throws Exception {
-        api  = montoyaApi;
-        view = scannerPanel;
+public class UploadScannerController implements ContextMenuItemsProvider {
+
+    public UploadScannerController(MontoyaApi montoyaApi, UploadScannerPanel scannerPanel,
+                                   UploadScannerMenuContext burpMenuContext) throws Exception {
+        api         = montoyaApi;
+        view        = scannerPanel;
+        menuContext = burpMenuContext;
+        scanTabList = new ArrayList<>();
 
         api.logging().logToOutput("Generating UI");
         addTabs();
         registerView();
+        registerMenuContext();
         api.logging().logToOutput("UI Generated");
     }
 
-    private final UploadScannerPanel     view;
-    private final MontoyaApi             api;
-    private       GlobalConfigController globalConfigController;
-    private       ConfigModel            globalConfigModel;
+    private final UploadScannerMenuContext menuContext;
+    private final UploadScannerPanel       view;
+    private final MontoyaApi               api;
+    private final List<ScanTab>            scanTabList;
+    private       ConfigModel              globalConfigModel;
+    private       GlobalConfigController   globalConfigController;
+
+    private void registerMenuContext() {
+        api.userInterface().registerContextMenuItemsProvider(menuContext);
+        menuContext.addEventListenerToMenuItem(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ScanTab newScanTab = new ScanTab(globalConfigModel, menuContext.getRequestResponse());
+                scanTabList.add(newScanTab);
+            }
+        });
+    }
 
     private void registerView() {
         api.userInterface().registerSuiteTab("Upload Scanner", view);
---
+++ src/main/java/view/tabs/ScanTab.java
@@ -0,0 +1,10 @@
+package view.tabs;
+
+import burp.api.montoya.http.message.HttpRequestResponse;
+import model.ConfigModel;
+
+public class ScanTab {
+
+    public ScanTab(ConfigModel globalConfigModel, HttpRequestResponse requestResponse) {
+    }
+}
