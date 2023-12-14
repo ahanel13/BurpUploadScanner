eb9e0b8a42c7b8f9323bcba88987bae836fd6b6e
Anthony Hanel <ahanel13@gmail.com>
2023-12-14 16:03:22 -0700

    Massive overhaul. Completed the BaseConfig view template

--- src/main/java/view/templates/BaseConfigTemplate.java
+++ src/main/java/view/templates/BaseConfigTemplate.java
@@ -3,6 +3,8 @@ package view.templates;
 import model.utilities.ResourceLoader;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -11,36 +13,19 @@ import java.util.Properties;
 //todo: get rid of magic strings
 public class BaseConfigTemplate extends JPanel {
 
-//  private final Object _constraints;
-
   public BaseConfigTemplate() throws IOException {
     super(); // Call the parent class constructor without BoxLayout
     setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); // Set BoxLayout here
-    setBackground(new Color(165, 110, 110));
+    setBorder(new EmptyBorder(20,20,20,20));
 
     initializeUIComponents();
-    _properties          = ResourceLoader.loadPropertyFile(BASE_CONFIG_TEMPLATE_PROPERTIES);
-//    _constraints         = getComponentConstraints();
-    _uniformCompSize = new Dimension(200, 50);
+    _properties = ResourceLoader.loadPropertyFile(BASE_CONFIG_TEMPLATE_PROPERTIES);
 
-    // Config Groups: Scanning Options
     addFileOptions();
-    addThrottleTimeGroup();
+    getThrottleTimeGroup();
     addPayloadOptions();
     addSleepOptions();
     addGeneralOptions();
-    addFooterUi();
-  }
-
-  @Override
-  public Dimension getPreferredSize() {
-    return new Dimension(PREFERRED_WIDTH, PREFERRED_HEIGHT);
-  }
-
-  private void getUniformSize(Component component) {
-    component.setPreferredSize(_uniformCompSize);
-    component.setMinimumSize(_uniformCompSize);
-    component.setMaximumSize(_uniformCompSize);
   }
 
   public String getThrottleValue() {
@@ -107,15 +92,15 @@ public class BaseConfigTemplate extends JPanel {
     _throttleValue.setText(String.valueOf(throttleValue));
   }
 
+  ///////////////////////////////////////
   // CONSTANTS
-  private static final int MIN_WIDTH        = 500;
-  private static final int MIN_HEIGHT       = 500;
-  private static final int MAX_WIDTH        = 1000;
-  private static final int MAX_HEIGHT       = 1000;
-  private static final int PREFERRED_WIDTH  = 500;
-  private static final int PREFERRED_HEIGHT = 500;
-  private static final int STD_JTEXT_COLS   = 3;
+  ///////////////////////////////////////
+  private static final Dimension STD_INPUT_SIZE = new Dimension(200, 30);
+  private static final int       STD_JTEXT_COLS = 3;
+  private static final Border    STD_BORDER     = new EmptyBorder(5, 5, 5, 5);
 
+
+  // Keys
   private static final String BASE_CONFIG_TEMPLATE_PROPERTIES = "BaseConfigView.properties";
   private static final String WGET_CURL_LABEL                 = "wget.curl.label";
   private static final String REPLACE_FILE_NAME               = "replace.file.name";
@@ -124,6 +109,8 @@ public class BaseConfigTemplate extends JPanel {
   private static final String ADD_TO_LOGGING                  = "add.to.logging.tab";
   private static final String THROTTLE_TIME                   = "throttle.time";
   private static final String SLEEP_TIME_LABEL                = "sleep.time.label";
+
+  private final Properties _properties;
 
   // UI components
   private JPanel _generalOptions;
@@ -138,7 +125,7 @@ public class BaseConfigTemplate extends JPanel {
   private JPanel  _footerButtons;
   private JButton _resetBtn;
   private JButton _saveBtn;
-  
+
   // UI inputs
   private JCheckBox  _addToLoggingChkBox;
   private JCheckBox  _replaceContentType;
@@ -148,62 +135,107 @@ public class BaseConfigTemplate extends JPanel {
   private JTextField _throttleValue;
   private JTextField _sleepTime;
 
-  private final Properties _properties;
-  private final Dimension  _uniformCompSize;
 
   private void addPayloadOptions() {
-    _payloadOptions.setLayout(new BorderLayout(0, 0));
+    _payloadOptions.setLayout(new BoxLayout(_payloadOptions, BoxLayout.Y_AXIS));
+    _payloadOptions.setBorder(STD_BORDER);
+    _payloadOptions.setAlignmentX(CENTER_ALIGNMENT);
+    _payloadOptions.add(getWgetCurlOpt());
     add(_payloadOptions);
+  }
 
+  private JPanel getWgetCurlOpt(){
     _wgetCurlPayloads = new JCheckBox();
     _wgetCurlPayloads.setHorizontalTextPosition(10);
     _wgetCurlPayloads.setText(_properties.getProperty(WGET_CURL_LABEL));
-    _wgetCurlPayloads.setMnemonic(' ');
-    _wgetCurlPayloads.setDisplayedMnemonicIndex(10);
-    _payloadOptions.add(_wgetCurlPayloads, BorderLayout.CENTER);
+
+      JPanel opts = getCheckBoxBorderJPanel();
+      opts.add(_wgetCurlPayloads, BorderLayout.EAST);
+      return  opts;
   }
 
   private void addFileOptions() {
     _fileOptions.setLayout(new BoxLayout(_fileOptions, BoxLayout.Y_AXIS));
+    _fileOptions.setBorder(STD_BORDER);
+    _fileOptions.add(getReplaceFileSizeOpt());
+    _fileOptions.add(getReplaceFileNameOpt());
+    _fileOptions.add(getReplaceFileContentOpt());
     add(_fileOptions);
+  }
 
+  private JPanel getReplaceFileSizeOpt(){
     _replaceFileSize = new JCheckBox();
     _replaceFileSize.setHorizontalTextPosition(10);
     _replaceFileSize.setSelected(true);
     _replaceFileSize.setText(_properties.getProperty(REPLACE_FILE_SIZE));
-    _fileOptions.add(_replaceFileSize);
 
+    JPanel fileSizeOpt = getCheckBoxBorderJPanel();
+    fileSizeOpt.add(_replaceFileSize, BorderLayout.EAST);
+    return fileSizeOpt;
+  }
+
+  private JPanel getReplaceFileNameOpt() {
     _replaceFileName = new JCheckBox();
     _replaceFileName.setHorizontalTextPosition(10);
     _replaceFileName.setSelected(true);
     _replaceFileName.setText(_properties.getProperty(REPLACE_FILE_NAME));
-    _fileOptions.add(_replaceFileName);
 
+    JPanel replaceFileNameOpt = getCheckBoxBorderJPanel();
+    replaceFileNameOpt.add(_replaceFileName, BorderLayout.EAST);
+
+    return replaceFileNameOpt;
+  }
+
+  private JPanel getReplaceFileContentOpt() {
     _replaceContentType = new JCheckBox();
     _replaceContentType.setHorizontalTextPosition(10);
     _replaceContentType.setSelected(true);
     _replaceContentType.setText(_properties.getProperty(REPLACE_CONTENT_TYPE));
-    _fileOptions.add(_replaceContentType);
+
+    JPanel replaceFileContOpt = getCheckBoxBorderJPanel();
+    replaceFileContOpt.add(_replaceContentType, BorderLayout.EAST);
+
+    return replaceFileContOpt;
+  }
+
+  private JPanel getCheckBoxBorderJPanel(){
+    JPanel tempPanel = new JPanel();
+    tempPanel.setLayout(new BorderLayout());
+    tempPanel.setAlignmentX(CENTER_ALIGNMENT);
+    tempPanel.setMaximumSize(STD_INPUT_SIZE);
+    return tempPanel;
   }
 
   private void addGeneralOptions() {
-    _generalOptions = new JPanel();
-    _generalOptions.setLayout(new BorderLayout(0, 0));
+    _generalOptions.setLayout(new BoxLayout(_generalOptions, BoxLayout.Y_AXIS));
+    _generalOptions.setBorder(STD_BORDER);
+    _generalOptions.add(getAddToLoggingOpt());
+    _generalOptions.add(getThrottleTimeGroup());
     add(_generalOptions);
-
-    _addToLoggingChkBox = new JCheckBox();
-    _addToLoggingChkBox.setHorizontalTextPosition(10);
-    _addToLoggingChkBox.setSelected(true);
-    _addToLoggingChkBox.setText(_properties.getProperty(ADD_TO_LOGGING));
-    _generalOptions.add(_addToLoggingChkBox, BorderLayout.CENTER);
   }
 
-  private void addThrottleTimeGroup() {
+  private JPanel getAddToLoggingOpt(){
+      _addToLoggingChkBox = new JCheckBox();
+      _addToLoggingChkBox.setHorizontalTextPosition(10);
+      _addToLoggingChkBox.setSelected(true);
+      _addToLoggingChkBox.setText(_properties.getProperty(ADD_TO_LOGGING));
+
+      JPanel opt = getCheckBoxBorderJPanel();
+      opt.add(_addToLoggingChkBox);
+      return opt;
+  }
+
+  private JPanel getTextFileGroupPanel(){
+    JPanel tempPanel = new JPanel();
+    tempPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
+    tempPanel.setMaximumSize(STD_INPUT_SIZE);
+    tempPanel.setAlignmentX(CENTER_ALIGNMENT);
+    return tempPanel;
+  }
+
+  private JPanel getThrottleTimeGroup() {
     // Panel Group
-    final JPanel throttleTimeGroup = new JPanel();
-    throttleTimeGroup.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-    throttleTimeGroup.setMinimumSize(_uniformCompSize);
-    throttleTimeGroup.setAlignmentX(Component.CENTER_ALIGNMENT);
+    final JPanel throttleTimeGroup = getTextFileGroupPanel();
 
     _throttleTimeLabel = new JLabel();
     _throttleTimeLabel.setText(_properties.getProperty(THROTTLE_TIME));
@@ -212,14 +244,11 @@ public class BaseConfigTemplate extends JPanel {
     _throttleValue = new JTextField(STD_JTEXT_COLS);
     throttleTimeGroup.add(_throttleValue);
 
-    add(throttleTimeGroup);
+    return throttleTimeGroup;
   }
 
   private void addSleepOptions() {
-    // Panel Group
-    final JPanel sleepOptions = new JPanel();
-    sleepOptions.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
-    _payloadOptions.add(sleepOptions, BorderLayout.CENTER);
+    final JPanel sleepOptions = getTextFileGroupPanel();
 
     _sleepTimeLabel = new JLabel();
     _sleepTimeLabel.setText(_properties.getProperty(SLEEP_TIME_LABEL));
@@ -227,6 +256,8 @@ public class BaseConfigTemplate extends JPanel {
 
     _sleepTime = new JTextField(STD_JTEXT_COLS);
     sleepOptions.add(_sleepTime);
+
+    _payloadOptions.add(sleepOptions);
   }
 
   private void addFooterUi() {
@@ -245,24 +276,34 @@ public class BaseConfigTemplate extends JPanel {
   }
 
   private void initializeUIComponents() {
-    _fileOptions = new JPanel();
-    _fileOptions.setBackground(new Color(110, 165, 117));
-
-    _payloadOptions  = new JPanel();
-    _payloadOptions.setBackground(new Color(110, 117, 165));
-
+    _fileOptions    = new JPanel();
+    _payloadOptions = new JPanel();
     _generalOptions = new JPanel();
-    _generalOptions.setBackground(new Color(165, 110, 149));
-
-    _footerButtons = new JPanel();
-    _footerButtons.setBackground(new Color(0, 255, 145));
+    _footerButtons  = new JPanel();
   }
 
   public static void main(String[] args) throws IOException {
     JFrame frame = new JFrame("BaseConfigTemplate");
-    frame.setContentPane(new BaseConfigTemplate());
+    BaseConfigTemplate debugView = new BaseConfigTemplate();
+//    debugView.addColors();
+    frame.setContentPane(debugView);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.pack();
     frame.setVisible(true);
+  }
+
+  private void addColors() {
+    // Color constants
+    final Color FILE_OPTIONS_COLOR    = new Color(110, 165, 117);
+    final Color PAYLOAD_OPTIONS_COLOR = new Color(110, 117, 165);
+    final Color GENERAL_OPTIONS_COLOR = new Color(165, 110, 149);
+    final Color FOOTER_BUTTONS_COLOR  = new Color(0, 255, 145);
+    final Color BACKGROUND_COLOR      = new Color(165, 110, 110);
+
+    _fileOptions.setBackground(FILE_OPTIONS_COLOR);
+    _payloadOptions.setBackground(PAYLOAD_OPTIONS_COLOR);
+    _generalOptions.setBackground(GENERAL_OPTIONS_COLOR);
+    _footerButtons.setBackground(FOOTER_BUTTONS_COLOR);
+    setBackground(BACKGROUND_COLOR);
   }
 }
