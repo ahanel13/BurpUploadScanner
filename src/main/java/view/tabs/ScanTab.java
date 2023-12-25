e0403536d2e08eb403003cc71fc7d49fdf755295
Anthony Hanel <ahanel13@gmail.com>
2023-12-25 15:30:07 -0700

    Refactored ScanTab

--- src/main/java/view/tabs/ScanTab.java
+++ src/main/java/view/tabs/ScanTab.java
@@ -16,182 +16,195 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class ScanTab extends JPanel {
-    
-    public ScanTab(HttpRequestResponse httpRequestResponse, UserInterface userInterface) throws IOException {
-        _requestResponse = httpRequestResponse;
-        _apiUI           = userInterface;
-        setLayout(new BorderLayout());
-        
-        // Initialize the left and right panels
-        JScrollPane leftPanel      = initializeLeftPanel();
-        JSplitPane  rightSplitPane = initializeRightSplitPane();
-        
-        // Create the main split pane with left and right components
-        JSplitPane mainSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, rightSplitPane);
-        
-        add(mainSplitPane, BorderLayout.CENTER);
+
+  ////////////////////////////////////////
+  // PUBLIC FUNCTIONS
+  ////////////////////////////////////////
+  public ScanTab(HttpRequestResponse httpRequestResponse, UserInterface userInterface) throws IOException {
+    _requestResponse = httpRequestResponse;
+    _apiUI           = userInterface;
+    setLayout(new BorderLayout());
+
+    // Initialize the left and right panels
+    JScrollPane leftPanel      = initializeLeftPanel();
+    JSplitPane  rightSplitPane = initializeRightSplitPane();
+
+    // Create the main split pane with left and right components
+    JSplitPane mainSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, rightSplitPane);
+
+    add(mainSplitPane, BorderLayout.CENTER);
+  }
+
+  ////////////////////////////////////////
+  // PUBLIC METHODS
+  ////////////////////////////////////////
+  public BaseConfigTemplate baseConfigTemplate() {return _baseConfig;}
+
+  // REDownLoader GETTERS
+  ////////////////////////////////////////
+  public String preflightEndpoint() {return _reDownloader.preflightEndpointInput.getText();}
+  public boolean replaceBackslash() {return _reDownloader.replaceBackslash.isSelected();}
+  public String getStartMarker()    {return _reDownloader.startMarker.getText();}
+  public String getEndMarker()      {return _reDownloader.endMarker.getText();}
+  public String getPrefix()         {return _reDownloader.prefix.getText();}
+  public String getSuffix()         {return _reDownloader.suffix.getText();}
+  public String getStaticUrl()      {return _reDownloader.staticUrl.getText();}
+
+  // Listener SETTERS
+  ////////////////////////////////////////
+  public void addSendPreflightReqListener(ActionListener l) {_actionPanel.preflightRequestBtn.addActionListener(l);}
+  public void addPreflightEndpointListener(DocumentListener l) {
+    _reDownloader.preflightEndpointInput.getDocument()
+        .addDocumentListener(l);
+  }
+  public void addReplaceBackslashListener(ActionListener l) {_reDownloader.replaceBackslash.addActionListener(l);}
+  public void addStartMarkerListener(DocumentListener l) {
+    _reDownloader.startMarker.getDocument()
+        .addDocumentListener(l);
+  }
+  public void addEndMarkerListener(DocumentListener l) {
+    _reDownloader.endMarker.getDocument()
+        .addDocumentListener(l);
+  }
+  public void addPrefixListener(ActionListener l)          {_reDownloader.prefix.addActionListener(l);}
+  public void addSuffixListener(ActionListener l)          {_reDownloader.suffix.addActionListener(l);}
+  public void addStaticUrlListener(ActionListener l)       {_reDownloader.staticUrl.addActionListener(l);}
+  public void setStartMarkerBackground(Color color)        {_reDownloader.startMarker.setBackground(color);}
+  public void setEndMarkerBackground(Color color)          {_reDownloader.endMarker.setBackground(color);}
+  public void addSendDownloadReqListener(ActionListener l) {_actionPanel.reDownloaderBtn.addActionListener(l);}
+
+  // HttpEditor Update Functions
+  ////////////////////////////////////////
+  public void updatePreflightWindows(HttpRequestResponse requestResponse) {
+    _prefliReqEditor.setRequest(requestResponse.request());
+    _prefliResEditor.setResponse(requestResponse.response());
+    _editorPanels.setEnabledAt(editorTabs.preflightRequest.ordinal(), true);
+    _editorPanels.setEnabledAt(editorTabs.preflightResponse.ordinal(), true);
+  }
+
+  public void updatePreflightWindow(HttpRequest request) {
+    _prefliReqEditor.setRequest(request);
+    _editorPanels.setEnabledAt(editorTabs.preflightRequest.ordinal(), true);
+    _editorPanels.updateUI();
+    _actionPanel.preflightRequestBtn.setEnabled(true);
+  }
+
+  public void updateReDownloadWindows(HttpRequestResponse requestResponse) {
+    _downReqEditor.setRequest(requestResponse.request());
+    _downResEditor.setResponse(requestResponse.response());
+    _editorPanels.setEnabledAt(editorTabs.downloadRequest.ordinal(), true);
+    _editorPanels.setEnabledAt(editorTabs.downloadResponse.ordinal(), true);
+  }
+
+  public void setReDownloadEditor(HttpRequest request) {
+    _downReqEditor.setRequest(request);
+    if (request.equals(HttpRequest.httpRequest()))
+      _editorPanels.setEnabledAt(editorTabs.downloadRequest.ordinal(), false);
+    else {
+      _editorPanels.setEnabledAt(editorTabs.downloadRequest.ordinal(), true);
+      _actionPanel.reDownloaderBtn.setEnabled(true);
     }
-    
-    
-    ////////////////////////////////////////
-    // PUBLIC METHODS
-    ////////////////////////////////////////
-    public BaseConfigTemplate baseConfigTemplate() {return _baseConfig;}
-    
-    // REDownLoader GETTERS
-    ////////////////////////////////////////
-    public String preflightEndpoint() {return _reDownloader.preflightEndpointInput.getText();}
-    public boolean replaceBackslash() {return _reDownloader.replaceBackslash.isSelected();}
-    public String getStartMarker()    {return _reDownloader.startMarker.getText();}
-    public String getEndMarker()      {return _reDownloader.endMarker.getText();}
-    public String getPrefix()         {return _reDownloader.prefix.getText();}
-    public String getSuffix()         {return _reDownloader.suffix.getText();}
-    public String getStaticUrl()      {return _reDownloader.staticUrl.getText();    }
-    
-    // Listener SETTERS
-    ////////////////////////////////////////
-    public void addSendPreflightReqListener(ActionListener l)     { _actionPanel.preflightRequestBtn.addActionListener(l); }
-    public void addPreflightEndpointListener(DocumentListener l)  { _reDownloader.preflightEndpointInput.getDocument().addDocumentListener(l); }
-    public void addReplaceBackslashListener(ActionListener l)     { _reDownloader.replaceBackslash.addActionListener(l); }
-    public void addStartMarkerListener(DocumentListener l)        { _reDownloader.startMarker.getDocument().addDocumentListener(l); }
-    public void addEndMarkerListener(DocumentListener l)          { _reDownloader.endMarker.getDocument().addDocumentListener(l); }
-    public void addPrefixListener(ActionListener l)               { _reDownloader.prefix.addActionListener(l); }
-    public void addSuffixListener(ActionListener l)               { _reDownloader.suffix.addActionListener(l); }
-    public void addStaticUrlListener(ActionListener l)            { _reDownloader.staticUrl.addActionListener(l); }
-    public void setStartMarkerBackground(Color color)             { _reDownloader.startMarker.setBackground(color); }
-    public void setEndMarkerBackground(Color color)               { _reDownloader.endMarker.setBackground(color); }
-    public void addSendDownloadReqListener(ActionListener l)      { _actionPanel.reDownloaderBtn.addActionListener(l); }
-    
-    // HttpEditor Update Functions
-    ////////////////////////////////////////
-    public void updatePreflightWindows(HttpRequestResponse requestResponse) {
-        _prefliReqEditor.setRequest(requestResponse.request());
-        _prefliResEditor.setResponse(requestResponse.response());
-        _editorPanels.setEnabledAt(editorTabs.preflightRequest.ordinal(), true);
-        _editorPanels.setEnabledAt(editorTabs.preflightResponse.ordinal(), true);
+    _editorPanels.updateUI();
+  }
+
+  public void setReDownloadSelection(String match) {
+    HttpResponseEditor response = (preflightEndpoint().isBlank()
+        ? _origResEditor : _prefliResEditor);
+
+    response.setSearchExpression(match);
+  }
+
+  // Misc
+  ////////////////////////////////////////
+  public void displayMessage(String message)              {JOptionPane.showMessageDialog(this, message);}
+  public void setStaticUrlBackground(Color color)         {_reDownloader.staticUrl.setBackground(color);}
+  public void setPreflightEndpointBackground(Color color) {_reDownloader.preflightEndpointInput.setBackground(color);}
+
+  ////////////////////////////////////////
+  // PRIVATE FIELDS
+  ////////////////////////////////////////
+  private final HttpRequestResponse  _requestResponse;
+  private final UserInterface        _apiUI;
+  private       ActionPanelTemplate  _actionPanel;
+  private       BaseConfigTemplate   _baseConfig;
+  private       ReDownloaderTemplate _reDownloader;
+  private       JPanel               _scanTabConfig;
+  private       JTabbedPane          _editorPanels;
+  private       HttpRequestEditor    _origReqEditor;
+  private       HttpResponseEditor   _origResEditor;
+  private       HttpRequestEditor    _prefliReqEditor;
+  private       HttpResponseEditor   _prefliResEditor;
+  private       HttpRequestEditor    _downReqEditor;
+  private       HttpResponseEditor   _downResEditor;
+
+
+  private enum editorTabs {
+    uploadRequest, uploadResponse, preflightRequest, preflightResponse,
+    downloadRequest, downloadResponse
+  }
+
+  ////////////////////////////////////////
+  // PRIVATE METHODS
+  ////////////////////////////////////////
+
+  // UI initalization
+  ////////////////////////////////////////
+  private JScrollPane initializeLeftPanel() throws IOException {
+    // Main Left Pane
+    _scanTabConfig = new JPanel();
+    _scanTabConfig.setLayout(new BoxLayout(_scanTabConfig, BoxLayout.Y_AXIS));
+
+    _baseConfig   = new BaseConfigTemplate();
+    _reDownloader = new ReDownloaderTemplate();
+
+    // Add more components as needed
+    _scanTabConfig.add(_baseConfig);
+    _scanTabConfig.add(_reDownloader);
+
+    // Wrap in JScrollPane for scrollability
+    JScrollPane pane = new JScrollPane(_scanTabConfig);
+    pane.getVerticalScrollBar().setUnitIncrement(20);
+    pane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+    return pane;
+  }
+
+  private JSplitPane initializeRightSplitPane() {
+    // Action Section (Upper Right Side)
+    _actionPanel = new ActionPanelTemplate();
+
+    // Request editor Section (Lower Right Side)
+    _editorPanels = getRequestEditorPanels();
+
+    // Splitting the right side vertically
+    JSplitPane rightSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, _actionPanel, _editorPanels);
+    rightSplitPane.setDividerLocation(0.20);
+
+    return rightSplitPane;
+  }
+
+  private JTabbedPane getRequestEditorPanels() {
+    JTabbedPane editorPane = new JTabbedPane();
+    _origReqEditor   = _apiUI.createHttpRequestEditor();
+    _origResEditor   = _apiUI.createHttpResponseEditor();
+    _prefliReqEditor = _apiUI.createHttpRequestEditor();
+    _prefliResEditor = _apiUI.createHttpResponseEditor();
+    _downReqEditor   = _apiUI.createHttpRequestEditor();
+    _downResEditor   = _apiUI.createHttpResponseEditor();
+
+    _origReqEditor.setRequest(_requestResponse.request());
+    _origResEditor.setResponse(_requestResponse.response());
+
+    editorPane.addTab("Upload Request", _origReqEditor.uiComponent());
+    editorPane.addTab("Upload Response", _origResEditor.uiComponent());
+    editorPane.addTab("Preflight Request", _prefliReqEditor.uiComponent());
+    editorPane.addTab("Preflight Response", _prefliResEditor.uiComponent());
+    editorPane.addTab("Redownload Request", _downReqEditor.uiComponent());
+    editorPane.addTab("Redownload Response", _downResEditor.uiComponent());
+
+    for (int i = 2; i < editorPane.getTabCount(); i++) {
+      editorPane.setEnabledAt(i, false);
     }
-    
-    public void updatePreflightWindow(HttpRequest request) {
-        _prefliReqEditor.setRequest(request);
-        _editorPanels.setEnabledAt(editorTabs.preflightRequest.ordinal(), true);
-        _editorPanels.updateUI();
-        _actionPanel.preflightRequestBtn.setEnabled(true);
-    }
-    
-    public void updateReDownloadWindows(HttpRequestResponse requestResponse) {
-        _downReqEditor.setRequest(requestResponse.request());
-        _downResEditor.setResponse(requestResponse.response());
-        _editorPanels.setEnabledAt(editorTabs.downloadRequest.ordinal(), true);
-        _editorPanels.setEnabledAt(editorTabs.downloadResponse.ordinal(), true);
-    }
-    
-    public void setReDownloadEditor(HttpRequest request) {
-        _downReqEditor.setRequest(request);
-        if (request.equals(HttpRequest.httpRequest()))
-            _editorPanels.setEnabledAt(editorTabs.downloadRequest.ordinal(), false);
-        else {
-            _editorPanels.setEnabledAt(editorTabs.downloadRequest.ordinal(), true);
-            _actionPanel.reDownloaderBtn.setEnabled(true);
-        }
-        _editorPanels.updateUI();
-    }
-    
-    public void setReDownloadSelection(String match) {
-        HttpResponseEditor response  = (preflightEndpoint().isBlank()
-            ? _origResEditor : _prefliResEditor);
-        
-        response.setSearchExpression(match);
-    }
-    
-    public void displayMessage(String message) { JOptionPane.showMessageDialog(this, message);    }
-    public void setStaticUrlBackground(Color color)     {_reDownloader.staticUrl.setBackground(color);}
-    public void setPreflightEndpointBackground(Color color) {_reDownloader.preflightEndpointInput.setBackground(color);}
-    
-    ////////////////////////////////////////
-    // PRIVATE FIELDS
-    ////////////////////////////////////////
-    private final HttpRequestResponse  _requestResponse;
-    private final UserInterface        _apiUI;
-    private       ActionPanelTemplate  _actionPanel;
-    private       BaseConfigTemplate   _baseConfig;
-    private       ReDownloaderTemplate _reDownloader;
-    private       JPanel               _scanTabConfig;
-    private       JTabbedPane          _editorPanels;
-    private       HttpRequestEditor    _origReqEditor;
-    private       HttpResponseEditor   _origResEditor;
-    private       HttpRequestEditor    _prefliReqEditor;
-    private       HttpResponseEditor   _prefliResEditor;
-    private       HttpRequestEditor    _downReqEditor;
-    private       HttpResponseEditor   _downResEditor;
-    
-    
-    private enum editorTabs{
-        uploadRequest, uploadResponse, preflightRequest, preflightResponse,
-        downloadRequest, downloadResponse
-    }
-    
-    ////////////////////////////////////////
-    // PRIVATE METHODS
-    ////////////////////////////////////////
-    
-    // UI initalization
-    ////////////////////////////////////////
-    private JScrollPane initializeLeftPanel() throws IOException {
-        // Main Left Pane
-        _scanTabConfig = new JPanel();
-        _scanTabConfig.setLayout(new BoxLayout(_scanTabConfig, BoxLayout.Y_AXIS));
-        
-        _baseConfig   = new BaseConfigTemplate();
-        _reDownloader = new ReDownloaderTemplate();
-        
-        // Add more components as needed
-        _scanTabConfig.add(_baseConfig);
-        _scanTabConfig.add(_reDownloader);
-        
-        // Wrap in JScrollPane for scrollability
-        JScrollPane pane = new JScrollPane(_scanTabConfig);
-        pane.getVerticalScrollBar().setUnitIncrement(20);
-        pane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        return pane;
-    }
-    
-    private JSplitPane initializeRightSplitPane() {
-        // Action Section (Upper Right Side)
-        _actionPanel = new ActionPanelTemplate();
-        
-        // Request editor Section (Lower Right Side)
-        _editorPanels = getRequestEditorPanels();
-        
-        // Splitting the right side vertically
-        JSplitPane rightSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, _actionPanel, _editorPanels);
-        rightSplitPane.setDividerLocation(0.20);
-        
-        return rightSplitPane;
-    }
-    
-    private JTabbedPane getRequestEditorPanels(){
-        JTabbedPane        editorPane     = new JTabbedPane();
-        _origReqEditor   = _apiUI.createHttpRequestEditor();
-        _origResEditor   = _apiUI.createHttpResponseEditor();
-        _prefliReqEditor = _apiUI.createHttpRequestEditor();
-        _prefliResEditor = _apiUI.createHttpResponseEditor();
-        _downReqEditor   = _apiUI.createHttpRequestEditor();
-        _downResEditor   = _apiUI.createHttpResponseEditor();
-        
-        _origReqEditor.setRequest(_requestResponse.request());
-        _origResEditor.setResponse(_requestResponse.response());
-        
-        editorPane.addTab("Upload Request", _origReqEditor.uiComponent());
-        editorPane.addTab("Upload Response", _origResEditor.uiComponent());
-        editorPane.addTab("Preflight Request", _prefliReqEditor.uiComponent());
-        editorPane.addTab("Preflight Response", _prefliResEditor.uiComponent());
-        editorPane.addTab("Redownload Request", _downReqEditor.uiComponent());
-        editorPane.addTab("Redownload Response", _downResEditor.uiComponent());
-        
-        for(int i = 2; i < editorPane.getTabCount(); i++){
-            editorPane.setEnabledAt(i, false);
-        }
-        
-        return  editorPane;
-    }
+
+    return editorPane;
+  }
 }
